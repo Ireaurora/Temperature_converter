6e5b62f4f69dea9cac077c3b0532783a70906627
Ireaurora <ireaurora@gmail.com>
2020-04-22 09:19:00 +0100

    Made some minor improvements

--- temperature.js
+++ temperature.js
@@ -1,7 +1,24 @@
+function convert_to_Celsius(){
+    let x = document.getElementById("mytext_from").value;
+    let fahrenheit = (x*9/5) + 32;
+    document.getElementById("mytext_to").value = fahrenheit;
+    document.getElementById("unit").value = "Celsius";
+}
+function convert_to_fahrenheit(){
+    let x = document.getElementById("mytext_from").value;
+    let celsius = (x - 32) *5/9 ;
+    document.getElementById("mytext_to").value = celsius;
+    document.getElementById("unit").value = "Fahrenheit"
+}
+function restart(){
+    document.getElementById("mytext_from").value = "";
+    document.getElementById("mytext_to").value ="";
+}
+/* Function not used 
 function GetSelectedItem(el)
 {
     var e = document.getElementById(el);
     var strSel = "The Value is: " + e.options[e.selectedIndex].value + " and text is: " + e.options[e.selectedIndex].text;
     alert(strSel);
 }
-
+*/
