529e90937ab96cfe02416d15cbd0cf728535da8f
Ireaurora <ireaurora@gmail.com>
2020-06-03 12:07:53 +0100

    Styling changes

--- temperature.js
+++ temperature.js
@@ -1,24 +1,16 @@
 function convert_to_Celsius(){
     let x = document.getElementById("mytext_from").value;
     let fahrenheit = (x*9/5) + 32;
-    document.getElementById("mytext_to").value = fahrenheit;
-    document.getElementById("unit").value = "Celsius";
+    document.getElementById("mytext_to").innerHTML = fahrenheit + " celsius";
+    document.getElementById("final").innerHTML = "To Celsius :";
 }
 function convert_to_fahrenheit(){
     let x = document.getElementById("mytext_from").value;
     let celsius = (x - 32) *5/9 ;
-    document.getElementById("mytext_to").value = celsius;
-    document.getElementById("unit").value = "Fahrenheit"
+    document.getElementById("mytext_to").innerHTML = celsius + " fahrenheit";
+    document.getElementById("final").innerHTML = "To Fahrenheit :"
 }
 function restart(){
-    document.getElementById("mytext_from").value = "";
-    document.getElementById("mytext_to").value ="";
+    document.getElementById("mytext_from").innerHTML = "";
+    document.getElementById("mytext_to").innerHTML ="";
 }
-/* Function not used 
-function GetSelectedItem(el)
-{
-    var e = document.getElementById(el);
-    var strSel = "The Value is: " + e.options[e.selectedIndex].value + " and text is: " + e.options[e.selectedIndex].text;
-    alert(strSel);
-}
-*/
